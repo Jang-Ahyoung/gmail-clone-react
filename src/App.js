bf654c373cda6bec754f93f6495c75d533c2d7f3
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-01 14:44:49 +0900

    🍰 creat sidebar & option component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-
+import Sidebar from './components/SideBar/sidebar';
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Let's build gmail!!</h1>
+      <Sidebar />
     </div>
   );
 }
