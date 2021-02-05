8294fea819c817b6c7f9dab995a47213d78476a0
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-05 12:13:52 +0900

    📧 creat & modify email component

--- src/components/EmailRow/EmailRow.jsx
+++ src/components/EmailRow/EmailRow.jsx
@@ -1,10 +1,12 @@
 import { IconButton,  Checkbox} from '@material-ui/core'
 import { LabelImportantOutlined, StarBorderOutlined } from '@material-ui/icons'
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import './EmailRow.css'
 function EmailRow({title, subject, description, time}) {
+    const history = useHistory();
     return (
-        <div className="emailRow">
+        <div onClick={()=> history.push('/mail')} className="emailRow">
             <div className="emailRow__options">
                 <Checkbox/>
                 <IconButton>
--- src/components/Mail/Mail.jsx
+++ src/components/Mail/Mail.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 import './Mail.css'
+import { useHistory } from 'react-router-dom';
+import { IconButton } from '@material-ui/core'
+import { ArrowBack, MoveToInbox, Error, Delete, Email, WatchLater, CheckCircle, LabelImportant, MoreVert, UnfoldMore, Print, ExitToApp} from '@material-ui/icons'
+
 function Mail() {
+    const history = useHistory();
     return (
         <div className="mail">
-            
+            <div className="mail__tools">
+                <div className="mail__toolsLeft">
+                <IconButton onClick={()=>history.push('/')}>
+                        <ArrowBack/>
+                    </IconButton>
+                    <IconButton>
+                        <MoveToInbox/>
+                    </IconButton>
+                    <IconButton>
+                        <Error/>
+                    </IconButton>
+                    <IconButton>
+                        <Delete/>
+                    </IconButton>
+                    <IconButton>
+                        <Email/>
+                    </IconButton>
+                    <IconButton>
+                        <WatchLater/>
+                    </IconButton>
+                    
+                    <IconButton>
+                        <CheckCircle/>
+                    </IconButton>
+                    <IconButton>
+                        <LabelImportant/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+                <div className="mail__toolsRight">
+                    <IconButton>
+                        <UnfoldMore/>
+                    </IconButton>
+                    <IconButton>
+                        <Print/>
+                    </IconButton>
+                    <IconButton>
+                        <ExitToApp/>
+                    </IconButton>
+                </div>
+                </div>
+            <div className="mail__body">
+                <div className="mail__bodyHeader">
+                    <h2>Subject</h2>
+                    <LabelImportant className="mail__important"/>
+                    <p>Title</p>
+                    <p className="mail__time">10 pm</p>
+                </div>
+                <div className="mail__message">
+                    <p>This is a message</p>
+                </div>
+            </div>
         </div>
     )
 }
