1d7eb3eed6c29dd27b0fc685c7ca24cd6d89ae5a
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-08 21:30:05 +0900

    📧 connect detail use redux to email row

--- src/components/EmailRow/EmailRow.jsx
+++ src/components/EmailRow/EmailRow.jsx
@@ -2,11 +2,20 @@ import { IconButton,  Checkbox} from '@material-ui/core'
 import { LabelImportantOutlined, StarBorderOutlined } from '@material-ui/icons'
 import React from 'react'
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './EmailRow.css'
-function EmailRow({title, subject, description, time}) {
+import { selectMail } from '../../features/mailSlice';
+function EmailRow({id, title, subject, description, time}) {
     const history = useHistory();
+    const dispatch = useDispatch();
+    const openMail = () => {
+        dispatch(selectMail({
+            id, title, subject, description, time
+        }));
+        history.push('/mail');
+    };
     return (
-        <div onClick={()=> history.push('/mail')} className="emailRow">
+        <div onClick={openMail} className="emailRow">
             <div className="emailRow__options">
                 <Checkbox/>
                 <IconButton>
--- src/components/Mail/Mail.jsx
+++ src/components/Mail/Mail.jsx
@@ -3,9 +3,12 @@ import './Mail.css'
 import { useHistory } from 'react-router-dom';
 import { IconButton } from '@material-ui/core'
 import { ArrowBack, MoveToInbox, Error, Delete, Email, WatchLater, CheckCircle, LabelImportant, MoreVert, UnfoldMore, Print, ExitToApp} from '@material-ui/icons'
+import { useSelector } from 'react-redux';
+import { selectOpenMail } from '../../features/mailSlice';
 
 function Mail() {
     const history = useHistory();
+    const selectedMail = useSelector(selectOpenMail);
     return (
         <div className="mail">
             <div className="mail__tools">
@@ -53,13 +56,13 @@ function Mail() {
                 </div>
             <div className="mail__body">
                 <div className="mail__bodyHeader">
-                    <h2>Subject</h2>
+                    <h2>{selectedMail?.subject}</h2>
                     <LabelImportant className="mail__important"/>
-                    <p>Title</p>
-                    <p className="mail__time">10 pm</p>
+                    <p>{selectedMail?.title}</p>
+                    <p className="mail__time">{selectedMail?.time}</p>
                 </div>
                 <div className="mail__message">
-                    <p>This is a message</p>
+                    <p>{selectedMail?.description}</p>
                 </div>
             </div>
         </div>
