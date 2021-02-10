31f1db19893a7811c6b57747ac5c4c42a92d0664
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-10 20:30:51 +0900

    🎠 firebase with env variable

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,13 +1,9 @@
 import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPXsrkcZ0aJ9lxNRUPFmY-_OcAVUC1kk0",
-    authDomain: "clone-ang.firebaseapp.com",
-    projectId: "clone-ang",
-    storageBucket: "clone-ang.appspot.com",
-    messagingSenderId: "367590296284",
-    appId: "1:367590296284:web:8864cc523ec8f4a2704219",
-    measurementId: "G-92QWM29FDH"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
